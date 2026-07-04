f0758cf6611258cfa216fdbacce2e1c760c6f024
vince-smdt <vince.smdt@gmail.com>
2026-07-04 11:01:54 -0400

    Add defines to activate sender/receiver

--- config.hpp
+++ config.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#define DEVICE_1   // Sender
+// #define DEVICE_2   // Receiver
+
 #define D1_RX_PIN 2
 #define D1_TX_PIN 12
 #define D2_RX_PIN 4
@@ -8,3 +11,5 @@
 #define HALF_BIT_US 50
 
 #define MAX_PAYLOAD_SIZE 80
+
+// #define INJECT_ERROR
--- s6-app4.ino
+++ s6-app4.ino
@@ -1,6 +1,7 @@
 #include "config.hpp"
 #include "protocol.hpp"
 
+#ifdef DEVICE_1
 static const char* donnees_txt[] = {
     "Ligne 1 - 2026-06-17 10:00:01 | Temp: 22.4 C | Humidite: 45.2 % | Node: 01",
     "Ligne 2 - 2026-06-17 10:05:01 | Temp: 22.5 C | Humidite: 45.1 % | Node: 01",
@@ -11,19 +12,25 @@ static const char* donnees_txt[] = {
 static const int total_paquets = sizeof(donnees_txt) / sizeof(donnees_txt[0]);
 
 static protocol::Sender sender;
-static protocol::Receiver receiver;
-
-static uint8_t rxBuffer[512];
-static volatile unsigned long txStartUs, txEndUs, rxStartUs, rxEndUs;
+static volatile unsigned long txStartUs, txEndUs;
 static volatile bool txDone = false;
-static volatile bool rxDone = false;
-static volatile bool startBenchmark = true;
 static volatile size_t totalBytes = 0;
 
 void txTask(void* pvParameters);
-void rxTask(void* pvParameters);
 void onEdgeD1();
+#endif // DEVICE_1
+
+#ifdef DEVICE_2
+static protocol::Receiver receiver;
+static uint8_t rxBuffer[512];
+static volatile unsigned long rxStartUs, rxEndUs;
+static volatile bool rxDone = false;
+
+void rxTask(void* pvParameters);
 void onEdgeD2();
+#endif // DEVICE_2
+
+static volatile bool startBenchmark = true;
 
 void setup() {
     Serial.begin(115200);
@@ -31,53 +38,73 @@ void setup() {
 
     Serial.println("\n=== Protocol Benchmark ===");
 
-    // D1_TX(12) -> D2_RX(4)  |  D2_TX(14) -> D1_RX(2)
+#ifdef DEVICE_1
     sender.begin(D1_TX_PIN, D1_RX_PIN, HALF_BIT_US);
-    receiver.begin(D2_TX_PIN, D2_RX_PIN, HALF_BIT_US);
-    receiver.setBuffer(rxBuffer, sizeof(rxBuffer));
 
     pinMode(D1_RX_PIN, INPUT);
     pinMode(D1_TX_PIN, OUTPUT);
+    digitalWrite(D1_TX_PIN, LOW);
+
+    xTaskCreatePinnedToCore(txTask, "txTask", 4096, NULL, 1, NULL, 1);
+    attachInterrupt(digitalPinToInterrupt(D1_RX_PIN), onEdgeD1, CHANGE);
+#endif
+
+#ifdef DEVICE_2
+    receiver.begin(D2_TX_PIN, D2_RX_PIN, HALF_BIT_US);
+    receiver.setBuffer(rxBuffer, sizeof(rxBuffer));
+
     pinMode(D2_RX_PIN, INPUT);
     pinMode(D2_TX_PIN, OUTPUT);
-
-    digitalWrite(D1_TX_PIN, LOW);
     digitalWrite(D2_TX_PIN, LOW);
 
     xTaskCreatePinnedToCore(rxTask, "rxTask", 4096, NULL, 1, NULL, 0);
-    xTaskCreatePinnedToCore(txTask, "txTask", 4096, NULL, 1, NULL, 1);
-
-    attachInterrupt(digitalPinToInterrupt(D1_RX_PIN), onEdgeD1, CHANGE);
     attachInterrupt(digitalPinToInterrupt(D2_RX_PIN), onEdgeD2, CHANGE);
+#endif
 }
 
 void loop() {
-    if (txDone && rxDone) {
+#ifdef DEVICE_1
+    if (txDone) {
         startBenchmark = false;
         txDone = false;
-        rxDone = false;
 
-        unsigned long txElapsed = txEndUs - txStartUs;
-        unsigned long rxElapsed = rxEndUs - rxStartUs;
-        float txSec = txElapsed / 1e6f;
-        float rxSec = rxElapsed / 1e6f;
+        unsigned long elapsed = txEndUs - txStartUs;
+        float sec = elapsed / 1e6f;
 
-        Serial.printf("\n--- Benchmark Results ---\n");
-        Serial.printf("Packets: %d  Total bytes: %u\n", total_paquets, totalBytes);
-        Serial.printf("Sender:   %lu us (%.3f s)  ->  %.2f B/s\n", txElapsed, txSec, totalBytes / txSec);
-        Serial.printf("Receiver: %lu us (%.3f s)  ->  %.2f B/s\n", rxElapsed, rxSec, totalBytes / rxSec);
+        Serial.printf("\n--- Sender Results ---\n");
+        Serial.printf("Bytes: %u  Time: %lu us (%.3f s)  ->  %.2f B/s\n",
+                      totalBytes, elapsed, sec, totalBytes / sec);
 
         vTaskDelay(5000);
         startBenchmark = true;
     }
+#endif
+
+#ifdef DEVICE_2
+    if (rxDone) {
+        startBenchmark = false;
+        rxDone = false;
+
+        size_t received = receiver.available();
+        unsigned long elapsed = rxEndUs - rxStartUs;
+        float sec = elapsed / 1e6f;
+
+        Serial.printf("\n--- Receiver Results ---\n");
+        Serial.printf("Bytes: %u  Time: %lu us (%.3f s)  ->  %.2f B/s\n",
+                      received, elapsed, sec, received / sec);
+
+        vTaskDelay(5000);
+        startBenchmark = true;
+    }
+#endif
 }
 
+#ifdef DEVICE_1
 void txTask(void* pvParameters) {
     char data[512];
 
     while (true) {
         while (!startBenchmark) vTaskDelay(10);
-
         vTaskDelay(100);
 
         size_t offset = 0;
@@ -91,7 +118,9 @@ void txTask(void* pvParameters) {
         Serial.printf("Sending %u bytes in %d packets...\n", offset, total_paquets);
 
         txStartUs = micros();
+#ifdef INJECT_ERROR
         sender.enableErrorInjection(3);
+#endif
         sender.sendSession((const uint8_t*)data, offset);
         txEndUs = micros();
 
@@ -99,10 +128,16 @@ void txTask(void* pvParameters) {
     }
 }
 
+void onEdgeD1() {
+    bool level = gpio_get_level((gpio_num_t)D1_RX_PIN);
+    sender.onEdge(level);
+}
+#endif // DEVICE_1
+
+#ifdef DEVICE_2
 void rxTask(void* pvParameters) {
     while (true) {
         while (!startBenchmark) vTaskDelay(10);
-
         vTaskDelay(50);
         receiver.reset();
         rxStartUs = micros();
@@ -119,12 +154,8 @@ void rxTask(void* pvParameters) {
     }
 }
 
-void onEdgeD1() {
-    bool level = gpio_get_level((gpio_num_t)D1_RX_PIN);
-    sender.onEdge(level);
-}
-
 void onEdgeD2() {
     bool level = gpio_get_level((gpio_num_t)D2_RX_PIN);
     receiver.onEdge(level);
 }
+#endif // DEVICE_2
